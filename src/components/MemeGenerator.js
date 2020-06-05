31e7a277d1f5b776b6da9bf76745467215f8cdda
Martinlud <hectormartinluduena@gmail.com>
2020-06-05 20:57:28 -0300

    header and memegenerator components

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MemeGenerator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <h1>MEME GENERATOR</h1>
+         );
+    }
+}
+ 
+export default MemeGenerator;
