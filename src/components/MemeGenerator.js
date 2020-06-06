df8e9258ee55b6a598b59aff8d9bc4e3e33d7095
Martinlud <hectormartinluduena@gmail.com>
2020-06-05 21:44:41 -0300

    handled values from input

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -9,6 +9,7 @@ class MemeGenerator extends Component {
             randomImg: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
         }
+        this.handleChange = this.handleChange.bind(this)
     }
 
     componentDidMount() {
@@ -22,9 +23,32 @@ class MemeGenerator extends Component {
             })
     }
 
+    handleChange(e){
+        const {name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() { 
         return ( 
-            <h1>MEME GENERATOR</h1>
+            <form>
+                <input 
+                    name='topText'
+                    type='text'
+                    placeholder= 'Top Text'
+                    onChange={this.handleChange}
+                    value={this.state.topText}
+                />
+                <input 
+                    name='bottomText'
+                    type='text'
+                    placeholder= 'Bottom Text'
+// recibe el objeto evento como parametro, si quiero pasar otra cosa debo llamar la funcion onChange={()=> this.handleChange(otroValor)}
+                    onChange={this.handleChange} 
+                    value={this.state.bottomText}
+                />
+            </form>
          );
     }
 }
