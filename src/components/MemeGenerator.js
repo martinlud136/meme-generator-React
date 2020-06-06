56e4d464b0790d4bf235a98ca8e888d9404570cd
Martinlud <hectormartinluduena@gmail.com>
2020-06-05 21:15:07 -0300

    state and fetch call

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react';
 class MemeGenerator extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  
+            topText: '',
+            bottomText: '',
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
     }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response=> response.json())
+            .then(response=>{
+                const {memes} = response.data
+                this.setState({
+                    allMemeImgs: memes
+                })
+            })
+    }
+
     render() { 
         return ( 
             <h1>MEME GENERATOR</h1>
